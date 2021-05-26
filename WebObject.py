53b9bf492e68e3f6941d3d485a4724d6ead500a9
Frank Fernandez <frafernandez@softekexport.com>
2021-05-26 12:08:33 -0400

    Script completed. Able to download all the office scripts form the url used in the script.

--- WebObject.py
+++ WebObject.py
@@ -1,4 +1,4 @@
 class WebObject:
-  def __init__(self, fileName, urlAddress):
-    self.fileName = fileName
-    self.urlAddress = urlAddress
+    def __init__(self, fileName, urlAddress):
+        self.fileName = fileName
+        self.urlAddress = urlAddress
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Scraper for the office scripts
 import requests
 from bs4 import BeautifulSoup
-
+import os
 from WebObject import WebObject
 
+#region GLOBAL VARIABLES
 BASE_URL = 'https://transcripts.foreverdreaming.org/viewforum.php?f=574'
 BASE_BASE_URL = 'https://transcripts.foreverdreaming.org/'
 PAGES = '&start='
@@ -11,55 +12,113 @@ PAGINATION_QUANTITY = 25
 CURRENT_PAGE = 0
 MAX_PAGINATION = 175
 PAGE = ""
-allatags = []
+ALL_A_TAGS_FROM_EPISODE_LIST = []
 
-officeObjects = []
+OFFICE_OBJECTS = []
 
+# Directory
+DIRECTORY = "The Office Scripts"
+
+# Parent Directory path
+PARENT_DIR = "./"
+
+# Path
+DIRECTORY_PATH = os.path.join(PARENT_DIR, DIRECTORY)
+
+#endregion
+
+#region METHODS
+
+#Getting the tags from the entire list of episodes and storing them in a global array
 def parseHtml(page):
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    allatags.extend(collect_all_a_tags(soup))
+    ALL_A_TAGS_FROM_EPISODE_LIST.extend(collect_all_a_tags(soup))
 
-    #for title in allatags:
-        #link = title.find('a')['href']
-
-        #print(title.text , title['href'], end='\n')
-
+# Collect al tags that have the episode link and name
 def collect_all_a_tags(soupResult):
     return soupResult.find_all('a', class_='topictitle')
 
 
-def generate_page():
+#Generating a url depending on the page the loop is on. The difference in each url is
+# the pagination number that happens to be 25 more than the previous page, starting at 0
+def generate_url():
     if CURRENT_PAGE == 0:
         return BASE_URL
     else:
         newUrl = BASE_URL + PAGES
         return newUrl + str(CURRENT_PAGE)
 
+#Processing the raw tags into managable objects
 def convertToObject(unstructured):
     for records in unstructured:
-        officeObjects.append(WebObject(records.text, records['href']))
+        OFFICE_OBJECTS.append(WebObject(records.text, records['href']))
 
+#Downloading the tags containing the script information
 def downloadScriptFor(episode):
+
+    print(f'Generating script file for {episode.fileName}', '\n')
     episode = BASE_BASE_URL + episode.urlAddress[2:]
     downloadFile = requests.get(episode)
     soup = BeautifulSoup(downloadFile.content, 'html.parser')
     dialog = soup.find_all('p')
-    #for test in dialog:
-        #print(test)
 
-# Press the green button in the gutter to run the script.
+
+    return dialog
+
+#Generating all script files
+def GenerateScriptFiles():
+
+    for episode in OFFICE_OBJECTS:
+        if episode.fileName == 'Please Read Updates: Take the 2021 Challenge!':
+            continue
+
+        downloadedDialog = downloadScriptFor(episode)
+
+        cleanName = episode.fileName.replace('/', "")
+        cleanName = cleanName.replace('?', "")
+        cleanName = cleanName.replace('*', "")
+
+        with open(f"{DIRECTORY_PATH}/{cleanName}.txt", "x", encoding="utf-8") as filehandle:
+            for line in downloadedDialog:
+                filehandle.write(f'{line.text}\n')
+
+
+def GenerateDirectory():
+    # Creating the directory to store the script files once downloaded
+    try:
+        if os.path.exists(DIRECTORY_PATH):
+            os.rmdir(DIRECTORY_PATH)
+
+        os.mkdir(DIRECTORY_PATH)
+        print("Directory '% s' created" % DIRECTORY)
+
+    except OSError as error:
+        print(error)
+
+#endregion
+
+#MAIN
 if __name__ == '__main__':
-    while CURRENT_PAGE <= MAX_PAGINATION:
-        url = generate_page()
 
-        PAGE = requests.get(url)
-        parseHtml(PAGE)
-        CURRENT_PAGE += PAGINATION_QUANTITY
+    GenerateDirectory()
 
-    #Converts all the unstructured html tags to objects
-    convertToObject(allatags)
+    try:
+        while CURRENT_PAGE <= MAX_PAGINATION:
+            url = generate_url()
 
-    downloadScriptFor(officeObjects[1])
+            PAGE = requests.get(url)
+            parseHtml(PAGE)
+            CURRENT_PAGE += PAGINATION_QUANTITY
 
-    print(len(officeObjects))
+        # Converts all the unstructured html tags to objects
+        convertToObject(ALL_A_TAGS_FROM_EPISODE_LIST)
+
+        GenerateScriptFiles()
+
+        print('Scripts downloaded')
+
+    except OSError as error:
+        print(error)
+
+
