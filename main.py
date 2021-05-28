3735a81f705dc53c73946e37856d2c75939971cf
Frank Fernandez <frafernandez@softekexport.com>
2021-05-28 12:29:08 -0400

    Script completed. Able to download all the office scripts form the url used in the script.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Scraper for the office scripts
+# Thanks for https://transcripts.foreverdreaming.org for
 import requests
 from bs4 import BeautifulSoup
 import os
 from WebObject import WebObject
 
-#region GLOBAL VARIABLES
+# region GLOBAL VARIABLES
 BASE_URL = 'https://transcripts.foreverdreaming.org/viewforum.php?f=574'
 BASE_BASE_URL = 'https://transcripts.foreverdreaming.org/'
 PAGES = '&start='
@@ -13,7 +14,6 @@ CURRENT_PAGE = 0
 MAX_PAGINATION = 175
 PAGE = ""
 ALL_A_TAGS_FROM_EPISODE_LIST = []
-
 OFFICE_OBJECTS = []
 
 # Directory
@@ -24,67 +24,67 @@ PARENT_DIR = "./"
 
 # Path
 DIRECTORY_PATH = os.path.join(PARENT_DIR, DIRECTORY)
+# endregion
 
-#endregion
+# region METHODS
 
-#region METHODS
-
-#Getting the tags from the entire list of episodes and storing them in a global array
-def parseHtml(page):
+# Getting the tags from the entire list of episodes and storing them in a global array
+def parse_html(page):
     soup = BeautifulSoup(page.content, 'html.parser')
 
     ALL_A_TAGS_FROM_EPISODE_LIST.extend(collect_all_a_tags(soup))
 
+
 # Collect al tags that have the episode link and name
-def collect_all_a_tags(soupResult):
-    return soupResult.find_all('a', class_='topictitle')
+def collect_all_a_tags(soup_result):
+    return soup_result.find_all('a', class_='topictitle')
 
 
-#Generating a url depending on the page the loop is on. The difference in each url is
+# Generating a url depending on the page the loop is on. The difference in each url is
 # the pagination number that happens to be 25 more than the previous page, starting at 0
 def generate_url():
     if CURRENT_PAGE == 0:
         return BASE_URL
     else:
-        newUrl = BASE_URL + PAGES
-        return newUrl + str(CURRENT_PAGE)
+        new_url = BASE_URL + PAGES
+        return new_url + str(CURRENT_PAGE)
 
-#Processing the raw tags into managable objects
-def convertToObject(unstructured):
+
+# Processing the raw tags into manageable objects
+def convert_to_object(unstructured):
     for records in unstructured:
         OFFICE_OBJECTS.append(WebObject(records.text, records['href']))
 
-#Downloading the tags containing the script information
-def downloadScriptFor(episode):
 
+# Downloading the tags containing the script information
+def download_script_for(episode):
     print(f'Generating script file for {episode.fileName}', '\n')
     episode = BASE_BASE_URL + episode.urlAddress[2:]
-    downloadFile = requests.get(episode)
-    soup = BeautifulSoup(downloadFile.content, 'html.parser')
+    download_file = requests.get(episode)
+    soup = BeautifulSoup(download_file.content, 'html.parser')
     dialog = soup.find_all('p')
-
 
     return dialog
 
-#Generating all script files
-def GenerateScriptFiles():
 
+# Generating all script files
+def generate_script_files():
     for episode in OFFICE_OBJECTS:
         if episode.fileName == 'Please Read Updates: Take the 2021 Challenge!':
             continue
 
-        downloadedDialog = downloadScriptFor(episode)
+        downloaded_dialog = download_script_for(episode)
 
-        cleanName = episode.fileName.replace('/', "")
-        cleanName = cleanName.replace('?', "")
-        cleanName = cleanName.replace('*', "")
+        clean_name = episode.fileName.replace('/', "")
+        clean_name = clean_name.replace('?', "")
+        clean_name = clean_name.replace('*', "")
 
-        with open(f"{DIRECTORY_PATH}/{cleanName}.txt", "x", encoding="utf-8") as filehandle:
-            for line in downloadedDialog:
-                filehandle.write(f'{line.text}\n')
+        with open(f"{DIRECTORY_PATH}/{clean_name}.txt", "x", encoding="utf-8") as file_handler:
+            for line in downloaded_dialog:
+                file_handler.write(f'{line.text}\n')
 
 
-def GenerateDirectory():
+def generate_directory():
     # Creating the directory to store the script files once downloaded
     try:
         if os.path.exists(DIRECTORY_PATH):
@@ -93,32 +93,31 @@ def GenerateDirectory():
         os.mkdir(DIRECTORY_PATH)
         print("Directory '% s' created" % DIRECTORY)
 
-    except OSError as error:
-        print(error)
+    except OSError as err:
+        print(err)
 
-#endregion
 
-#MAIN
+# endregion
+
+# MAIN
 if __name__ == '__main__':
 
-    GenerateDirectory()
+    generate_directory()
 
     try:
         while CURRENT_PAGE <= MAX_PAGINATION:
             url = generate_url()
 
             PAGE = requests.get(url)
-            parseHtml(PAGE)
+            parse_html(PAGE)
             CURRENT_PAGE += PAGINATION_QUANTITY
 
         # Converts all the unstructured html tags to objects
-        convertToObject(ALL_A_TAGS_FROM_EPISODE_LIST)
+        convert_to_object(ALL_A_TAGS_FROM_EPISODE_LIST)
 
-        GenerateScriptFiles()
+        generate_script_files()
 
         print('Scripts downloaded')
 
     except OSError as error:
         print(error)
-
-
